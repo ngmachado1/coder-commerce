3b34e6743f2bd33c51aeec32e89ed87b5ef375e7
Júnior Nogueira <haroldognjunior@gmail.com>
2021-03-12 19:15:04 -0600

    agregando listeners

--- products.js
+++ products.js
@@ -41,6 +41,7 @@ function Products(productos) {
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <h6>$${article.price}</h6>
+
                 <p class="card-text">Some quick example text to build on the card title and make up
                     the bulk
                     of the card's content.</p>
